5176812c3110c8700a4262fb8494e03a7248718a
Kemy-Blann <maxime.catteau13@gmail.com>
2015-09-30 18:15:50 +0200

    1er push

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Ça marche pour moi !"); //Kemy
+		/*
+		 * 
+		 * Test pour voir si les push marchent
+		 * 30/09 - 18h03
+		 * 
+		 */
+		for(int i = 1; i <= 10; i++){
+			Thread.sleep(1000);
+			System.out.println("Phrase n°"+i);
+		}
+		System.out.println("Fin du programme !");
+	}
+}
